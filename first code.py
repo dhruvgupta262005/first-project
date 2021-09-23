c301cedf2da67aba0dd482ea0c9c759ef5042e14
dhruvgupta262005 <91242784+dhruvgupta262005@users.noreply.github.com>
2021-09-23 15:25:30 +0530

    Add files via upload

---
+++ first code.py
@@ -0,0 +1,12 @@
+#this is my first comment to the reader
+print("i am writing a code") #abhi sikh hi raha hoo code likhna
+print("this is my 2nd code line")
+"""  we cannot use (#) symboll in front of print statement as 
+the interpritter of python will count it as an comment statement """
+#example
+"""print("this statement will not be printed")"""
+print("this is my 3rd line","this is practice")
+print("this is third statement also",end=" ")# we can write anything between  the two {""} signs and it will join the next line to the first line.
+print("and i am learning\nsomething new\tHI")# here \n means take a new line and \t means take a tab
+print("c\\titan") # we have to use \\ signes doubel becouse else the system will count it as \n which is an escape sequence.
+print("THANKS FOR READING MY CODE")
